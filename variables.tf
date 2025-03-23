7c1b878ecb83ad90f75da713d582c989c191a82f
jhunter7 <jarredhunter6@gmail.com>
2025-03-22 21:54:53 -0500

    decrease instance count

--- variables.tf
+++ variables.tf
@@ -9,6 +9,6 @@ variable "instance_config" {
     instance_type = "t2.micro"
     region        = "us-east-1a"
     ami           = "ami-0f9de6e2d2f067fca"
-    count         = 5
+    count         = 3
   }
 }
